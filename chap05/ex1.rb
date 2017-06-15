d8bfab19dc6a01b9ed4467482762bceb148dc049
James Ransome <jamesransome@msn.com>
2017-06-15 11:57:24 +0000

    chapter 5 exercise 1

---
+++ chap05/ex1.rb
@@ -0,0 +1,8 @@
+#Full name greeting
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+mid_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + mid_name + " " + last_name + "!"
