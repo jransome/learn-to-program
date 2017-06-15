406626e78667c2ae9580f3f2d37b7c518a07f5db
James Ransome <jamesransome@msn.com>
2017-06-15 12:56:00 +0000

    chapter 6 exercise 3

---
+++ chap06/ex3.rb
@@ -0,0 +1,14 @@
+#Table of contents
+line_width = 60
+TOC_title = "Table of Contents"
+chapter1 = "Chapter 1: Getting Started"
+chapter1_page = "page  1"
+chapter2 = "Chapter 2: Numbers"
+chapter2_page = "page  9"
+chapter3 = "Chapter 3: Letters"
+chapter3_page = "page 13"
+
+puts TOC_title.center(line_width)
+puts(chapter1.ljust(line_width/2) + chapter1_page.rjust(line_width/2))
+puts(chapter2.ljust(line_width/2) + chapter2_page.rjust(line_width/2))
+puts(chapter3.ljust(line_width/2) + chapter3_page.rjust(line_width/2))
