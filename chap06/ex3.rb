ef1d26c08e982e037f839bc4704d2af258001521
James Ransome <jamesransome@msn.com>
2017-06-15 20:33:24 +0000

    renaming toc_title variable

--- chap06/ex3.rb
+++ chap06/ex3.rb
@@ -1,6 +1,6 @@
 #Table of contents
 line_width = 60
-TOC_title = "Table of Contents"
+toc_title = "Table of Contents"
 chapter1 = "Chapter 1: Getting Started"
 chapter1_page = "page  1"
 chapter2 = "Chapter 2: Numbers"
@@ -8,7 +8,7 @@ chapter2_page = "page  9"
 chapter3 = "Chapter 3: Letters"
 chapter3_page = "page 13"
 
-puts TOC_title.center(line_width)
+puts toc_title.center(line_width)
 puts(chapter1.ljust(line_width/2) + chapter1_page.rjust(line_width/2))
 puts(chapter2.ljust(line_width/2) + chapter2_page.rjust(line_width/2))
 puts(chapter3.ljust(line_width/2) + chapter3_page.rjust(line_width/2))
